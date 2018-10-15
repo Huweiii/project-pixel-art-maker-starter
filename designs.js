df8910a5bc232409e8b4448d8799765dc08c3d98
235496139@qq.com <235496139@qq.com>
2018-10-15 14:10:11 +0800

    Create table for PIXEL ARTMAKER

--- designs.js
+++ designs.js
@@ -1,10 +1,47 @@
 // Select color input
-// Select size input
+const color = document.getElementById('colorPicker');
+const table = document.getElementById('pixelCanvas');
 
-// When size is submitted by the user, call makeGrid()
+let w = document.getElementById('inputWidth').value;
+let h = document.getElementById('inputHeight').value;
+const btn = document.querySelector('#submit');
+
+btn.addEventListener('click', makeGrid);
 
 function makeGrid() {
+  //reset table
+  table.innerHTML = "";
 
-// Your code goes here!
+  w = document.getElementById('inputWidth').value;
+  h = document.getElementById('inputHeight').value;
+  // Add new element <tbody> and <td>
 
+  for(i=1; i<=h; i++) {
+    const addNewTr = document.createElement('tr');
+        for(j=1; j<=w; j++) {
+            const addNewTd = document.createElement('td');
+            addNewTr.appendChild(addNewTd);
+        } 
+    // tr-->table    
+    table.appendChild(addNewTr);
+  } 
 }
+
+//模拟点击一次
+btn.click();
+
+// Add event listener. get picked color.
+table.addEventListener('click', function(event){
+    let td = event.target;
+    // 查看nodeName
+    console.log('clicked' + event.target.nodeName);
+    // 查看backgroundColor
+    console.info("color=" + event.target.style.backgroundColor);
+
+    if(td.style.backgroundColor != '' && td.style.backgroundColor == color.value){
+        td.style.backgroundColor = '';
+    }else{
+        td.style.backgroundColor = color.value;
+    }   
+});
+
